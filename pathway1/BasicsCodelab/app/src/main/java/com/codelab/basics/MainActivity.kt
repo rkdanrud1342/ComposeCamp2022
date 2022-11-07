2cf768d51185c7be5b8c31c0f4e17fe5bd7d6ffe
강동현 <rkdanrud1342@gmail.com>
2022-11-07 17:21:42 +0900

    Text width를 fillMaxWidth로 설정

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -36,7 +37,10 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.fillMaxWidth()
+    ) {
         Text(
             text = "Hello $name!",
             modifier = Modifier.padding(24.dp)
