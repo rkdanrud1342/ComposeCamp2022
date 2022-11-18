988ac2c44cd2b115b33516e5e6defbe268fdcdc5
강동현 <rkdanrud1342@gmail.com>
2022-11-18 19:47:28 +0900

    상태 기반 UI

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -14,13 +12,14 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count = remember { mutableStateOf(0) }
-        Text(
-            text = "You've had ${count.value} glasses.",
-            modifier = modifier.padding(16.dp)
-        )
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+
         Button(
-            onClick = { count.value++ },
+            onClick = { count++ },
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text(text = "Add one")
