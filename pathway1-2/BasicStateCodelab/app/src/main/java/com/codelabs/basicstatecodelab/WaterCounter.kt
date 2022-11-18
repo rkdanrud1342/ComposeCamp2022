a0bb43b43089258e26240a6afc4031a30d6866f3
강동현 <rkdanrud1342@gmail.com>
2022-11-18 19:40:01 +0900

    Composable 함수의 메모리

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,24 +1,29 @@
 package com.codelabs.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
-    Button(
-        onClick = { count++ },
-        modifier = Modifier.padding(top = 8.dp)
-    ) {
-        Text(text = "Add one")
+    Column(modifier = modifier.padding(16.dp)) {
+        val count = remember { mutableStateOf(0) }
+        Text(
+            text = "You've had ${count.value} glasses.",
+            modifier = modifier.padding(16.dp)
+        )
+        Button(
+            onClick = { count.value++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
     }
 }
