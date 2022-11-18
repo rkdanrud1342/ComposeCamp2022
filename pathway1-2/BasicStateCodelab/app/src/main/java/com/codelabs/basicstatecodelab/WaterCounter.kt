34f8cd1f884c1a57221c889f7bdabe8baa4826d3
강동현 <rkdanrud1342@gmail.com>
2022-11-18 20:05:11 +0900

    상태 호이스팅

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -10,36 +10,29 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
-
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(all = 16.dp)) {
         if (count > 0) {
-            var showTask by rememberSaveable { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text(text = "You've had $count glasses.")
         }
 
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(
-                onClick = { count++ },
-                enabled = count < 10
-            ) {
-                Text("Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 8.dp)
-            ) {
-                Text(text = "Clear water count")
-            }
+        Button(
+            onClick = onIncrement,
+            enabled = count < 10,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Add one")
         }
     }
+}
+
+@Composable
+fun StatefulCount(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = { count++ }, modifier = modifier)
 }
