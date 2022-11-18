dcd9b0c843df3632ee219d952aa66ce8f0125389
강동현 <rkdanrud1342@gmail.com>
2022-11-18 23:21:47 +0900

    목록 사용

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,7 +1,6 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskItem.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskItem.kt
@@ -2,19 +2,38 @@ package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+
+@Preview
+@Composable
+fun WellnessTaskItemPreview() {
+    BasicStateCodelabTheme {
+        WellnessTaskItem(
+            taskName = "This is a task",
+            checked = false,
+            onCheckedChange = { },
+            onClose = { })
+    }
+}
 
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -23,11 +42,30 @@ fun WellnessTaskItem(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            modifier = Modifier.weight(1f).padding(start = 16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
             text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "close")
         }
     }
+}
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { checkedState = it },
+        onClose = {},
+        modifier = modifier
+    )
 }
